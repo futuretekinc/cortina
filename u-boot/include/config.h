3ae6630be9a5cca992228f4612b6b70832c1d432
futuretek <devweb@futuretek.co.kr>
2017-08-03 17:39:51 +0900

    first commit

---
+++ u-boot/include/config.h
@@ -0,0 +1,5 @@
+/* Automatically generated - do not edit */
+#define CONFIG_MK_FTM 1
+#define CONFIG_MK_100S 1
+#include <configs/ftm_100s.h>
+#include <asm/config.h>
---
+++ u-boot/include/config.mk
@@ -0,0 +1,5 @@
+ARCH   = arm
+CPU    = arm_cortexa9
+BOARD  = ftm-100s
+VENDOR = futuretek
+SOC    = goldengate
